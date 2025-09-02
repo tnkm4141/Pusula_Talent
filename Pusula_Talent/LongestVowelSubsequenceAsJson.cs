fb94604c94cfe86a545fda11ada18225498f5cc0
Kardelen Mutlu <tugcenurkardelen@gmail.com>
2025-09-02 13:34:51 +0300

    Add project files.

---
+++ Pusula_Talent/LongestVowelSubsequenceAsJson.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+
+namespace Pusula_Talent
+{
+    public static string LongestVowelSubsequenceAsJson(List<string> words)
+    {
+    }
+}
+
---
+++ Pusula_Talent/MaxIncreasingSubArrayAsJson.cs
@@ -0,0 +1,11 @@
+﻿using System.Linq;
+using System.Text.Json;
+using System.Collections.Generic;
+
+namespace Pusula_Talent
+{
+
+    public static string MaxIncreasingSubArrayAsJson(List<int> numbers)
+    {
+    }
+}
